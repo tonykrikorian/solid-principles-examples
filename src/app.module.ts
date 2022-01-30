09f291a1ff423ae1cd3bc77ced4014e4f7b2a5b1
Tony Krikorian <tony.krikorian@outlook.com>
2022-01-30 20:13:00 -0300

    Regularizar proyecto

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import BeerModule from './modules/beer/beer.module';
+import PoliciesModule from './modules/policies/policies.module';
 
 @Module({
-  imports: [BeerModule],
+  imports: [BeerModule, PoliciesModule],
 })
 export class AppModule {}
---
+++ src/modules/policies/services/models/classes/policy.model.ts
@@ -0,0 +1,6 @@
+/* eslint-disable prettier/prettier */
+export default interface Policy {
+  origen: string;
+  documentId: string;
+  policy: string;
+}
