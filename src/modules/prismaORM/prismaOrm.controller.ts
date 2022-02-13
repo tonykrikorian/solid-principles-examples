cff07bb3ba50df4eb0d19b0b082d6d6dca304ad8
Tony Krikorian <tony.krikorian@outlook.com>
2022-02-12 22:12:31 -0300

    insert in man y to many

--- src/modules/prismaORM/prismaOrm.controller.ts
+++ src/modules/prismaORM/prismaOrm.controller.ts
@@ -2,6 +2,7 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import PrismaService from 'src/prisma.service';
 import { AddCategoryRequest } from './services/models/requests/addCategorie.request';
+import { AddExpenseCategoryRequest } from './services/models/requests/addExpenseCategory.request';
 import { AddPeriodRequest } from './services/models/requests/AddPeriod.request';
 
 @Controller('database-example')
@@ -24,5 +25,38 @@ export default class PrismaOrmController {
         ...body,
       },
     });
+  }
+
+  @Post('add-category-expense')
+  public async addCategoryExpense(@Body() body: AddExpenseCategoryRequest) {
+    const { salary, id } = await this.prismaService.period.findFirst({
+      where: { period: body.period },
+    });
+    const finalAmmount = await this.calculateExpenseCategoryAmmount(
+      body.salaryPercentage,
+      salary,
+    );
+
+    return this.prismaService.expenseCategory.create({
+      data: {
+        salaryPercentage: body.salaryPercentage,
+        ammount: finalAmmount,
+        periods: {
+          create: [
+            {
+              periodId: id,
+            },
+          ],
+        },
+      },
+    });
+  }
+
+  private async calculateExpenseCategoryAmmount(
+    percentage: number,
+    salary: number,
+  ): Promise<number> {
+    const finalAmmount = Number(salary) * (percentage / 100);
+    return finalAmmount;
   }
 }
---
+++ src/modules/prismaORM/services/models/requests/addExpenseCategory.request.ts
@@ -0,0 +1,6 @@
+/* eslint-disable prettier/prettier */
+export interface AddExpenseCategoryRequest {
+  salaryPercentage: number;
+  categoryId: number;
+  period: string;
+}
