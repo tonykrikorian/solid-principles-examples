34c6b9cfd1f8d50ce022326e2081c6a152e6fd4a
Tony Krikorian <tony.krikorian@anschile.cl>
2022-03-04 21:21:02 -0300

    Add factory method

---
+++ src/modules/oop/services/models/classes/factory/factory.ts
@@ -0,0 +1,66 @@
+/* eslint-disable prettier/prettier */
+//Abstract Creator
+
+abstract class SaleFactory {
+  public abstract getSale(): ISale;
+}
+
+//Concrete creator
+class StoreSaleFactory extends SaleFactory {
+  constructor(private readonly extra: number) {
+    super();
+  }
+
+  public getSale(): ISale {
+    return new StoreSale(this.extra);
+  }
+}
+
+//Concrete creator
+class InternetSaleFactory extends SaleFactory {
+  constructor(private readonly tax: number, private readonly discount: number) {
+    super();
+  }
+  public getSale(): ISale {
+    return new InternetSale(this.tax, this.discount);
+  }
+}
+
+//Concrete product
+class StoreSale implements ISale {
+  private _extra: number;
+
+  constructor(extra: number) {
+    this._extra = extra;
+  }
+
+  sell(total: number): void {
+    console.log(`Total de la venta en tienda ${total + this._extra}`);
+  }
+}
+
+//Concrete product
+class InternetSale implements ISale {
+  constructor(
+    private readonly tax: number,
+    private readonly discount: number,
+  ) {}
+  sell(total: number): void {
+    console.log(
+      `La venta por internet es igual a ${total + this.tax - this.discount}`,
+    );
+  }
+}
+
+//Product
+interface ISale {
+  sell(total: number): void;
+}
+
+//Implementación
+
+const storeFactory: SaleFactory = new StoreSaleFactory(10);
+const internetSale: SaleFactory = new InternetSaleFactory(1, 2);
+
+storeFactory.getSale().sell(10);
+internetSale.getSale().sell(10);
