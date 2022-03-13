6c84a0386bde1d7ae99b584e4acd9b01a74246ac
Tony Krikorian <tony.krikorian@outlook.com>
2022-03-12 21:41:02 -0300

    Delete modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,8 @@
 /* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import BeerModule from './modules/beer/beer.module';
-import OopModule from './modules/oop/oop.module';
-import PrismaOrmModule from './modules/prismaORM/prismaOrm.module';
 
 @Module({
-  imports: [BeerModule, PrismaOrmModule, OopModule],
+  imports: [BeerModule],
 })
 export class AppModule {}
