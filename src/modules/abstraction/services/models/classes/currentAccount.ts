83736eff32347f561b46c72930d8a6f2e8144d03
tkrikoriam <tony.krikorian@outlook.com>
2022-06-18 14:52:18 -0400

    Refactor class name

--- src/modules/abstraction/services/models/classes/currentAccount.ts
+++ src/modules/abstraction/services/models/classes/currentAccount.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@nestjs/common';
 import AbstractAccountDeposit from './abstractAccountDeposits';
 
 @Injectable()
-export default class CurrentAccount extends AbstractAccountDeposit {
+export default class CurrentAccountDeposit extends AbstractAccountDeposit {
   protected depositIntoAccount() {
     console.log('Logica para deposito cuenta corriente');
   }
